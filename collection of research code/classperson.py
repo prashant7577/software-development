04c4535fe36862984efad9c7536fdf23a7d4dcd3
prashant7577 <167256508+prashant7577@users.noreply.github.com>
2024-04-19 19:28:45 +1200

    Create classperson.py

---
+++ collection of research code/classperson.py
@@ -0,0 +1,8 @@
+class Person:
+   def __init__(self, name, age):
+      self.name = name
+      self.age = age
+person1 = Person("Alice", 30)
+print(f"{person1.name} is {person1.age} years old.")
+
+  
